34f2b2e2a5b3d05dbf4f12e7bd41d8612cd38f80
dev ads mania <dev@mania.tn>
2020-11-24 16:42:45 +0100

    Implementing checkboxes

--- src/CheckBoxComponent/checkbox.js
+++ src/CheckBoxComponent/checkbox.js
@@ -1,33 +1,75 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 //Creating a class component who contains 5 checkboxes initilized as unchecked
-class CheckBox extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            allChecked: false,
-            list: [
-                {id:1, name: "item1", isChecked: false},
-                {id:2, name: "item2", isChecked: false},
-                {id:3, name: "item3", isChecked: false},
-                {id:4, name: "item4", isChecked: false},
-                {id:5, name: "item 5", isChecked: false},
-            ],
-        };
-    }
-// Displaying Checkboxes 
-    render() {
-        return (
-          <div>
-            <input/>
-            Select All
-            <br />
-          </div>
+class CheckBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allChecked: false,
+      list: [
+        { id: 1, name: "item1", isChecked: false },
+        { id: 2, name: "item2", isChecked: false },
+        { id: 3, name: "item3", isChecked: false },
+        { id: 4, name: "item4", isChecked: false },
+        { id: 5, name: "item 5", isChecked: false },
+      ],
+    };
+  }
+
+  // Handling chekboxes event on Selecting/Unselecting
+  handleChange = (e) => {
+    let itemName = e.target.name;
+    let checked = e.target.checked;
+    this.setState((prevState) => {
+      let { list, allChecked } = prevState;
+      if (itemName === "checkAll") {
+        allChecked = checked;
+        list = list.map((item) => ({ ...item, isChecked: checked }));
+      } else {
+        list = list.map((item) =>
+          item.name === itemName ? { ...item, isChecked: checked } : item
         );
+        allChecked = list.every((item) => item.isChecked);
       }
-    }
-    
-    ReactDOM.render(<CheckBox />, document.getElementById("root"));
+      return { list, allChecked };
+    });
+  };
   
-  export default CheckBox;
+  // Function to display Checkboxes
+  renderList = () => {
+    return this.state.list.map((item) => (
+      <div>
+        <input
+          key={item.id}
+          type="checkbox"
+          name={item.name}
+          value={item.name}
+          checked={item.isChecked}
+          onChange={this.handleChange}
+        />
+        <label>{item.name}</label>
+      </div>
+    ));
+  };
+  // Rendering Checkboxes
+  render() {
+    return (
+      <div>
+        <input
+          type="checkbox"
+          name="checkAll"
+          checked={this.state.allChecked}
+          onChange={this.handleChange}
+        />
+        Select All
+        <br />
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<CheckBox />, document.getElementById("root"));
+
+export default CheckBox;
